2c4d734c191ccc0a671a0bcd0887b2748f08a2af
Harishmalika <gharishece74@gmail.com>
2023-11-08 16:50:50 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def silkuka(request):
+    return HttpResponse('<h1><marque> pombala soku kakuthe da  <marque></h1>')
